623e96a5c77e96d9b00b7a65e8948a24d87d6896
shshwdr <cs.vivian04@gmail.com>
2021-01-07 00:40:21 -0600

    first attemp

---
+++ Assets/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public Move ball1;
+
+    public Move ball2;
+
+    Rigidbody2D ball1R;
+    Rigidbody2D ball2R;
+
+    bool isBall1Active;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isBall1Active = ball1.isActive;
+        ball1R = ball1.GetComponent<Rigidbody2D>();
+
+        ball2R = ball2.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Vector2 previousVelocity = -(isBall1Active ? ball1.previousVelocity() : ball2.previousVelocity());
+            ball1.toggleActive();
+            ball2.toggleActive();
+            isBall1Active = !isBall1Active;
+
+            if (isBall1Active)
+            {
+                ball1.setVelocity(previousVelocity);
+
+            }
+            else
+            {
+                ball2.setVelocity(previousVelocity);
+            }
+        }
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public Rigidbody2D another;
+    public bool isActive;
+    int speed = 80;
+    bool rotationDir = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        updateConstrains();
+    }
+    public Vector2 previousVelocity()
+    {
+        return rigidbody.velocity;
+    }
+    public void setVelocity(Vector2 velocity)
+    {
+        rigidbody.velocity = velocity;
+    }
+    void updateConstrains()
+    {
+        if (isActive)
+        {
+            rigidbody.constraints = RigidbodyConstraints2D.None;
+
+        }
+        else
+        {
+            rigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+           // rigidbody.velocity = Vector2.zero;
+            //rigidbody.a
+        }
+    }
+    public void toggleActive()
+    {
+        isActive = !isActive;
+        updateConstrains();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isActive)
+        {
+
+            Vector2 dir = (rigidbody.position - another.position).normalized;
+            Vector2 perpen = Vector2.Perpendicular(dir);
+            GetComponent<Rigidbody2D>().AddForce(perpen * speed);
+        }
+    }
+}
